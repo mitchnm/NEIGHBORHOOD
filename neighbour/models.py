0c3ec024342c30081983f55c96a7bfca91f99b95
mitchnm <mitchndungu21@gmail.com>
2019-07-05 15:43:49 +0300

    added necessary classes

--- neighbour/models.py
+++ neighbour/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
+    profile_pic = models.ImageField(upload_to = 'awards/',blank=True)
+    bio = models.CharField(max_length=250, null=True)
+    neighbourhood = models.ForeignKey('Neighbourhood')
+
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=60)
+    location = models.CharField(max_length=60)
+
+class Business(models.Model):
+    business_name = models.CharField(max_length=60)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
+    neighbourhood = models.ForeignKey('Neighbourhood')
+    business_email = models.CharField(max_length=100)
