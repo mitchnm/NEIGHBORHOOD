ae499c5bf80d2106d087c1a869c2f4ebe2f82c3a
mitchnm <mitchndungu21@gmail.com>
2019-07-05 16:22:57 +0300

    added registration function

--- neighbour/views.py
+++ neighbour/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Post, Profile, Neighbourhood, Post
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def welcome(request):
     return render(request, 'index.html')
--- neighbourproject/settings.py
+++ neighbourproject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'neighbour.apps.NeighbourConfig',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
