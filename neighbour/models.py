f32f98e28a5b34754416970545bbdeaeac4ca3c3
mitchnm <mitchndungu21@gmail.com>
2019-07-07 12:07:45 +0300

    added profile view

--- neighbour/models.py
+++ neighbour/models.py
@@ -8,15 +8,18 @@ class Profile(models.Model):
     bio = models.CharField(max_length=250, null=True)
     neighbourhood = models.ForeignKey('Neighbourhood')
 
+
 class Neighbourhood(models.Model):
     name = models.CharField(max_length=60)
     location = models.CharField(max_length=60)
+
 
 class Business(models.Model):
     business_name = models.CharField(max_length=60)
     user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
     neighbourhood = models.ForeignKey('Neighbourhood')
     business_email = models.CharField(max_length=100)
+
 
 class Post(models.Model):
     image = models.ImageField(upload_to = 'awards/')
--- neighbour/views.py
+++ neighbour/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from .models import Post, Profile, Neighbourhood, Post
+from .models import Post, Profile, Neighbourhood,Business
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def welcome(request):
     return render(request, 'index.html')
+
+@login_required(login_url='/accounts/login/')
+def profile(request,id):
+  post = Post.objects.filter(user_id=id)
+  current_user = request.user
+  user = User.objects.get(id=id)
+  try:
+    profile = profile.objects.get(user=id)
+  except ObjectDoesNotExist:
+    return render(request, 'profile.html')
+  return render(request, 'profile.html', {"post":post, "user":user, "profile":profile})
