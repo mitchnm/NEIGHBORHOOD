3e8f135105821c2020c7047d9d26eb1f285a6721
mitchnm <mitchndungu21@gmail.com>
2019-07-08 17:12:15 +0300

    edited tests

--- neighbour/tests.py
+++ neighbour/tests.py
@@ -19,7 +19,7 @@ class PostTestCase(TestCase):
 class ProfileTest(TestCase):
     def setUp(self):
         self.user = User.objects.create(id =1,username='a')
-        self.new_profile = Profile(name=self.user, profile_pic='media/instagram/photo.jpg',bio='I am awesome')
+        self.new_profile = Profile(name=self.user, profile_pic='media/instagram/photo.jpg',bio='I am awesome',id=2)
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_profile,Profile))
@@ -29,20 +29,20 @@ class ProfileTest(TestCase):
         profile = Profile.objects.all()
         self.assertTrue(len(profile)>0)
 
-class RatingtTest(TestCase):
+class NeighbourhoodtTest(TestCase):
     def setUp(self):
         self.user = User.objects.create(id =1,username='a')
-        self.newproject = Project(image='media/awards/Fashion.jpg',project_name='Fashion',project_description='Delicious',id =1,url='http://127.0.0.1:8000/',profile=self.user)
-        self.new_rating = Comment(project=self.newproject,user=self.user,design='2',usability='5',content='9',comment='You are awesome',)
+        self.newpost = Post(image='media/neighbourhood/Fashion.jpg',description='Fashion',id =1,id=3,profile=self.user)
+        self.new_neighbourhood = Neighbourhood(name='boom',picture='media/neighbourhood/Fashion.jpg')
    
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_rating,Comment))
+        self.assertTrue(isinstance(self.new_neighbourhood,Neighbourhood))
 
-class RatingtTest(TestCase):
+class BusinessTest(TestCase):
     def setUp(self):
         self.user = User.objects.create(id =1,username='a')
-        self.newproject = Project(image='media/awards/Fashion.jpg',project_name='Fashion',project_description='Delicious',id =1,url='http://127.0.0.1:8000/',profile=self.user)
-        self.new_rating = Comment(project=self.newproject,user=self.user,design='2',usability='5',content='9',comment='You are awesome',)
+        self.newpost = Post(image='media/neighbourhood/Fashion.jpg',description='Fashion',id =1,id=3,profile=self.user)
+        self.new_business = Business(image='media/neighbourhood/Fashion.jpg', )
    
     def test_instance(self):
         self.assertTrue(isinstance(self.new_rating,Comment))
